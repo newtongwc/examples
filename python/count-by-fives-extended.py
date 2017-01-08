d4f9c1c1d4552745a6b7198836ef2be3edb16350
David Miller <david@newtongwc.org>
2017-01-08 07:28:24 -0500

    Adds basic and extended versions of a simple programming exercise that counts up to 100 by fives.

---
+++ python/count-by-fives-extended.py
@@ -0,0 +1,5 @@
+# This program counts up to 100 by fives.
+# It was written as a Daily Programming Exercise.
+
+for i in range(5, 105, 5):
+    print "%d..." % i,
---
+++ python/count-by-fives.py
@@ -0,0 +1,20 @@
+# This program gets ready to play hide and seek by counting up by fives.
+# It was written as a Daily Programming Exercise.
+
+# Optional: use to pause between counts
+import time
+
+print "Now everybody try to find a good hiding place."
+print "This ol' tree is going to be the base."
+print "I'm going to close my eyes and hide my face"
+print "And count to a hundred by fives."
+print "Ready? Go!"
+print
+
+for i in range(5, 105, 5):
+    print "%d..." % i,
+    # Optional: pause between counts
+    time.sleep(0.5)
+
+print "\n"
+print "Ready or not, here I come!"
